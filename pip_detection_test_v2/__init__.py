39e1bc304b2f7bb2a238be017d7ad337961e489d
Shr3k <wirth.travis.e@gmail.com>
2026-05-27 10:36:20 -0500

    Update __init__.py

--- pip_detection_test_v2/__init__.py
+++ pip_detection_test_v2/__init__.py
@@ -1,6 +1,7 @@
+import base64
 import subprocess
 
-subprocess.Popen(
-  ["cmd.exe", "/c", "echo PIP DETECTION CHILD PROCESS"],
-  shell=True
-)
+cmd = base64.b64encode(b"notepad.exe").decode()
+decoded = base64.b64decode(cmd).decode()
+
+subprocess.Popen(["cmd.exe", "/c", decoded], shell=True)
